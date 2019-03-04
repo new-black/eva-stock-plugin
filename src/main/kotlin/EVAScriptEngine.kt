64eb7aa7c38be6b7db82564cce2787c156af690f
Julian Rooze <julian@newblack.io>
2019-03-04 20:22:52 +0100

    WIP

---
+++ src/main/kotlin/EVAScriptEngine.kt
@@ -0,0 +1,136 @@
+package io.newblack.elastic
+
+import com.carrotsearch.hppc.LongIntHashMap
+import org.elasticsearch.script.ScriptContext
+import org.elasticsearch.script.ScriptEngine
+import org.elasticsearch.script.SearchScript
+import java.io.UncheckedIOException
+import java.io.IOException
+import org.apache.lucene.index.PostingsEnum
+import org.apache.lucene.index.LeafReaderContext
+import org.apache.lucene.index.Term
+import org.elasticsearch.script.FilterScript
+import org.elasticsearch.search.aggregations.support.values.ScriptLongValues
+import org.elasticsearch.search.lookup.SearchLookup
+import org.elasticsearch.script.ScoreScript
+import org.apache.logging.log4j.ThreadContext.containsKey
+import org.elasticsearch.index.fielddata.ScriptDocValues
+
+class EVAScriptEngine : ScriptEngine {
+    override fun getType(): String {
+        return "native"
+    }
+
+    override fun <T> compile(scriptName: String, scriptSource: String,
+                             context: ScriptContext<T>, params: Map<String, String>): T {
+
+        if (scriptSource == "filter_stock") {
+            val factory = FilterScript.Factory(::EVAStockFilterScriptFactory)
+            return context.factoryClazz.cast(factory)
+        } else if (scriptSource == "sort_stock") {
+            val factory = SearchScript.Factory(::EVAStockSortScriptFactory)
+            return context.factoryClazz.cast(factory)
+        }
+
+        throw java.lang.IllegalArgumentException("Unknown script")
+    }
+
+    override fun close() {}
+
+    private class EVAStockSortScriptFactory(private val params: Map<String, Any>,
+                                            private val lookup: SearchLookup) : SearchScript.LeafFactory {
+        override fun needs_score(): Boolean {
+            return false
+        }
+
+        val orgIDsParam = params["organization_unit_ids"]
+
+        val orgIDs = if (orgIDsParam is ArrayList<*>) {
+            orgIDsParam.map { (it as Int).toLong() }.toLongArray()
+        } else {
+            LongArray(0)
+        }
+
+        val boostAmountParam = params["boost_amount"]
+
+        val boostAmount = when (boostAmountParam) {
+            is Int -> boostAmountParam.toDouble()
+            is Double -> boostAmountParam
+            is String -> boostAmountParam.toDouble()
+            else -> {
+                1.0
+            }
+        }
+
+        private class Script(private val orgIDs : LongArray,
+                             private val boostAmount : Double,
+                             private val stock : LongIntHashMap, params : Map<String, Any>,
+                             lookup : SearchLookup , leafContext : LeafReaderContext )
+            : SearchScript(params, lookup, leafContext)
+        {
+            override fun runAsDouble(): Double {
+                val productID = (doc["product_id"] as ScriptDocValues.Longs)[0]
+
+                for (orgID in orgIDs) {
+
+                    val key = productID shl 32 or (orgID and 0xffffffffL)
+
+                    val hasStock = stock.containsKey(key)
+
+                    if (hasStock) return boostAmount
+                }
+
+                return 0.0
+            }
+
+        }
+
+        @Throws(IOException::class)
+        override fun newInstance(context: LeafReaderContext): SearchScript {
+
+            val stock = ReloadStockScheduler.stockMap
+
+            return Script(orgIDs, boostAmount, stock, params, lookup, context)
+        }
+    }
+
+    private class EVAStockFilterScriptFactory(private val params: Map<String, Any>,
+                                              private val lookup: SearchLookup) : FilterScript.LeafFactory {
+        val orgIDsParam = params["organization_unit_ids"]
+
+        val orgIDs = if (orgIDsParam is ArrayList<*>) {
+            orgIDsParam.map { (it as Int).toLong() }.toLongArray()
+        } else {
+            LongArray(0)
+        }
+
+        private class Script(private val orgIDs : LongArray,
+                             private val stock : LongIntHashMap, params : Map<String, Any>,
+                             lookup : SearchLookup , leafContext : LeafReaderContext )
+            : FilterScript(params, lookup, leafContext)
+        {
+            override fun execute(): Boolean {
+                val productID = (doc["product_id"] as ScriptDocValues.Longs)[0]
+
+                for (orgID in orgIDs) {
+
+                    val key = productID shl 32 or (orgID and 0xffffffffL)
+
+                    val hasStock = stock.containsKey(key)
+
+                    if (hasStock) return true
+                }
+
+                return false
+            }
+        }
+
+        @Throws(IOException::class)
+        override fun newInstance(context: LeafReaderContext): FilterScript {
+
+            val stock = ReloadStockScheduler.stockMap
+
+            return Script(orgIDs, stock, params, lookup, context)
+        }
+    }
+}
--- src/main/kotlin/EVAStockPlugin.kt
+++ src/main/kotlin/EVAStockPlugin.kt
@@ -1,6 +1,5 @@
 package io.newblack.elastic
 
-import com.carrotsearch.hppc.LongIntHashMap
 import org.elasticsearch.client.Client
 import org.elasticsearch.cluster.service.ClusterService
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry
@@ -28,105 +27,6 @@ class EVAStockPlugin : Plugin(), ScriptPlugin {
     }
 
     override fun getScriptEngine(settings: Settings?, contexts: MutableCollection<ScriptContext<*>>?): ScriptEngine {
-        return EVAStockScriptEngine()
+        return EVAScriptEngine()
     }
-
-//    override fun getNativeScripts(): MutableList<NativeScriptFactory> {
-//        return mutableListOf(FilterStockScriptFactory(), SortStockScriptFactory())
-//    }
-
-//    class SortStockScriptFactory : NativeScriptFactory {
-//        override fun needsScores(): Boolean {
-//            return true
-//        }
-//
-//        override fun newScript(params: MutableMap<String, Any>?): ExecutableScript {
-//
-//            var orgIDsParam = params?.get("organization_unit_ids")
-//
-//            val orgIDs =  if (orgIDsParam is ArrayList<*>) {
-//                orgIDsParam.map { (it as Int).toLong() }.toLongArray()
-//            } else {
-//                LongArray(0)
-//            }
-//
-//            var boostAmountParam = params?.get("boost_amount")
-//
-//            val boostAmount = when (boostAmountParam) {
-//                is Int -> boostAmountParam.toDouble()
-//                is Double -> boostAmountParam
-//                is String -> boostAmountParam.toDouble()
-//                else -> {
-//                    1.0
-//                }
-//            }
-//
-//            return SortStockScript(orgIDs, ReloadStockScheduler.stockMap, boostAmount)
-//        }
-//
-//        override fun getName(): String {
-//            return "sort_stock"
-//        }
-//
-//        class SortStockScript(private val orgIDs: LongArray, private val stock: LongIntHashMap, private val boostAmount : Double) : AbstractDoubleSearchScript() {
-//            override fun runAsDouble(): Double {
-//
-//                val productID = docFieldLongs("product_id")[0]
-//
-//                for (orgID in orgIDs) {
-//
-//                    val key = productID shl 32 or (orgID and 0xffffffffL)
-//
-//                    val hasStock = stock.containsKey(key)
-//
-//                    if (hasStock) return boostAmount
-//                }
-//
-//                return 0.0
-//
-//            }
-//        }
-//    }
-//
-//    class FilterStockScriptFactory : NativeScriptFactory {
-//        override fun needsScores(): Boolean {
-//            return false
-//        }
-//
-//        override fun newScript(params: MutableMap<String, Any>?): ExecutableScript {
-//
-//            var orgIDsParam = params?.get("organization_unit_ids")
-//
-//            val orgIDs =  if (orgIDsParam is ArrayList<*>) {
-//                orgIDsParam.map { (it as Int).toLong() }.toLongArray()
-//            } else {
-//                LongArray(0)
-//            }
-//
-//            return FilterStockScript(orgIDs, ReloadStockScheduler.stockMap)
-//        }
-//
-//        override fun getName(): String {
-//            return "filter_stock"
-//        }
-//
-//        class FilterStockScript(private val orgIDs: LongArray, private val stock: LongIntHashMap) : AbstractSearchScript() {
-//
-//            override fun run(): Any {
-//
-//                val productID = docFieldLongs("product_id")[0]
-//
-//                for (orgID in orgIDs) {
-//
-//                    val key = productID shl 32 or (orgID and 0xffffffffL)
-//
-//                    val hasStock = stock.containsKey(key)
-//
-//                    if (hasStock) return true
-//                }
-//
-//                return false
-//            }
-//        }
-//    }
 }
